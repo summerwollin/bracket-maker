016d55b6bd216e29837d2665f19891dacc46df72
Summer Wollin <summer.wollin@gmail.com>
2016-05-23 18:57:48 -0600

    basic mongoose setup

--- model/brackets.js
+++ model/brackets.js
@@ -1,8 +1,7 @@
-var mongoose = require('mongoose');
-var blobSchema = new mongoose.Schema({
-  name: String,
-  badge: Number,
-  dob: { type: Date, default: Date.now },
-  isloved: Boolean
-});
-mongoose.model('Blob', blobSchema);
+// var mongoose = require('mongoose');
+// var blobSchema = new mongoose.Schema({
+//   name: String,
+//   badge: Number,
+//   isloved: Boolean
+// });
+// mongoose.model('Blob', blobSchema);
--- model/db.js
+++ model/db.js
@@ -1,2 +1,15 @@
 var mongoose = require('mongoose');
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/bracketsdb");
+
+var db = mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+  var kittySchema = mongoose.Schema({
+      name: String
+  });
+  var Kitten = mongoose.model('Kitten', kittySchema);
+  var silence = new Kitten({ name: 'Silence' });
+  console.log(silence.name);
+});
+
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/test');
