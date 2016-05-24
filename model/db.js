87401c0f4f8ce95df3d9e4388cdeeb62ba8c7278
Summer Wollin <summer.wollin@gmail.com>
2016-05-24 10:34:57 -0600

    heroku debug

--- model/db.js
+++ model/db.js
@@ -24,27 +24,27 @@ db.once('open', function() {
   var brackets = mongoose.model('brackets', bracketsSchema);
 
   //drop users model data before seeding
-  // mongoose.connection.db.dropCollection('users', function(err, result) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log('dropped users collection');
-  //   }
-  // });
-  // //insert seed data to users model
-  // users.find({}).exec(function (err, collection) {
-  //   console.log('length: ', collection.length);
-  //     if (collection.length === 0) {
-  //         users.create({ name: 'Summer' });
-  //         users.create({ name: 'Alan' });
-  //         users.create({ name: 'Mike' });
-  //         users.create({ name: 'Lincoln' });
-  //         users.create({ name: 'Jeff' });
-  //         users.create({ name: 'Akyuna' });
-  //         users.create({ name: 'Dize' });
-  //         users.create({ name: 'Matt' });
-  //     }
-  // });
+  mongoose.connection.db.dropCollection('users', function(err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('dropped users collection');
+    }
+  });
+  //insert seed data to users model
+  users.find({}).exec(function (err, collection) {
+    console.log('length: ', collection.length);
+      if (collection.length === 0) {
+          users.create({ name: 'Summer' });
+          users.create({ name: 'Alan' });
+          users.create({ name: 'Mike' });
+          users.create({ name: 'Lincoln' });
+          users.create({ name: 'Jeff' });
+          users.create({ name: 'Akyuna' });
+          users.create({ name: 'Dize' });
+          users.create({ name: 'Matt' });
+      }
+  });
 });
 
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/bracket');
